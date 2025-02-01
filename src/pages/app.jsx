aa043f02d4bf797a43d30c80b1cf1b2498e16710
Waqas Yahya <waqasyahya143s@gmail.com>
2025-02-01 18:20:23 +0500

    file structure

---
+++ src/pages/app.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'preact/hooks'
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    <h1>waqas</h1>
+    
+      
+     
+     
+     
+    </>
+  )
+}
---
+++ src/pages/islame.jsx
@@ -0,0 +1,7 @@
+function home() {
+    
+   return <h1>waqas</h1>
+    
+    
+}
+export default home;
